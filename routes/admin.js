1a51b8b44ee94752dfcbd95fa43fa3d2fc3ced97
MANUCHANDRAN94 <m4manu001@gmail.com>
2020-09-24 22:27:31 +0530

    admin route and view completed

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
+
   let products =[{
     name:"Mobile1",
     catagory:"mobile",
@@ -28,7 +29,16 @@ router.get('/', function(req, res, next) {
     image:"https://static.digit.in/product/thumb_160001_product_td_300.jpeg"
   }]
 
-  res.render('index', { products , admin:true });
+  res.render("admin/view-products",{products ,admin:true});
 });
+
+router.get('/add-product', function(req, res) {
+  res.render("admin/add-product" )
+})
+router.post('/add-product', function(req, res) {
+  console.log(req.body);
+  console.log(req.files.Image)
+  // res.render("admin/add-product" )
+})
 
 module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products =[{
+    name:"Mobile1",
+    catagory:"mobile",
+    description:"it is a mobile",
+    image:"https://static.digit.in/product/thumb_160001_product_td_300.jpeg"
+  },
+  {
+    name:"Mobile2",
+    catagory:"mobile",
+    description:"it is a mobile",
+    image:"https://static.digit.in/product/thumb_160001_product_td_300.jpeg"
+  },
+  {
+    name:"Mobile3",
+    catagory:"mobile",
+    description:"it is a mobile",
+    image:"https://static.digit.in/product/thumb_160001_product_td_300.jpeg"
+  },
+  {
+    name:"Mobile4",
+    catagory:"mobile",
+    description:"it is a mobile",
+    image:"https://static.digit.in/product/thumb_160001_product_td_300.jpeg"
+  }]
+
+  res.render('index', { products , admin:false });
+});
+
+module.exports = router;
